8a18205675c2c959527262944d41cbfc93cea8f8
RamDie <dievaldi@gmail.com>
2021-05-22 15:03:58 +0000

    6.4 contextos y mensajes

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -7,6 +7,8 @@ sap.ui.define([
 ],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.model.Filter} Filter
+     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
      */
     function (Controller, Filter, FilterOperator) {
         "use strict";
@@ -58,6 +60,12 @@ sap.ui.define([
                 var oModel = this.getView().getModel();
                 oModel.setProperty("/EmployeeId","");
                 oModel.setProperty("/CountryKey","");
+            },
+            showPostalCode: function(oEvent){
+                var itemPressed = oEvent.getSource();
+                var oContext = itemPressed.getBindingContext();
+                var objectContext = oContext.getObject();
+                sap.m.MessageToast.show(objectContext.PostalCode);
             }
         });
     });
